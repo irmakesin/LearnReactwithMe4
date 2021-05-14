00d849ac6ec5ad551a862b8a34efe4bfbea027b7
irmakesin <esinirmak95@gmail.com>
2021-05-15 00:26:44 +0300

    keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization:
-          'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
-      },
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
     });
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,29 +2,28 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: '' };
-  // onInputChange(event) {
-  //   console.log(event.target.value)
-  // }
 
   onFormSubmit = event => {
     event.preventDefault();
 
-    console.log(this.state.term);
-  }
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-          <label>Image Search</label>
-            <input type="text"
+            <label>Image Search</label>
+            <input
+              type="text"
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })} />
-            </div>
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
